9b6f0ceac7616a96790af8ce2dc172681c4e8701
Chong <aaa59891@gmail.com>
2017-05-24 08:11:32 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  console.log(`${now}: ${req.method} ${req.url}`);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('Maintenance.hbs', {
+    pageTitle: 'maintenance',
+    msg: 'maintenanece'
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMsg: "Hi chong!"
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Wrong url!'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
